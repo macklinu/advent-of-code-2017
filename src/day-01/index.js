43cd3149495c84f7c9b41bd707f3ef7447963d03
Macklin Underdown <macklinu@users.noreply.github.com>
2017-12-03 12:30:43 -0500

    Solve Day 1 Part 1

--- src/day-01/index.js
+++ src/day-01/index.js
@@ -1,1 +1,13 @@
-// todo
+function inverseCaptcha(input) {
+  let numbers = input.split('').map(n => parseInt(n, 10))
+  let length = numbers.length
+  return numbers.reduce((prev, curr, index) => {
+    let nextItem = numbers[(index + 1) % length]
+    if (curr == nextItem) {
+      return prev + curr
+    }
+    return prev
+  }, 0)
+}
+
+module.exports = inverseCaptcha
